0b07fe7d8aa8fa207383dad14d81cd5a3da1d4cd
Justin Kaseman <justinkaseman@live.com>
2017-11-12 19:04:04 -0800

    toggle displaying form

--- Homework-4/FontChooser.js
+++ Homework-4/FontChooser.js
@@ -2,20 +2,23 @@ class FontChooser extends React.Component {
 
     constructor(props) {
 	super(props);
+  this.state = {hidden : true};
     }
-    
+
+    toggle() {
+      this.setState( { hidden: !this.state.hidden } );
+    }
 
     render() {
 
 	return(
 	       <div>
-	       <input type="checkbox" id="boldCheckbox" hidden='true'/>
-	       <button id="decreaseButton" hidden='true'>-</button>
-	       <span id="fontSizeSpan" hidden='true'>{this.props.size}</span>
-	       <button id="increaseButton" hidden='true'>+</button>
-	       <span id="textSpan">{this.props.text}</span>
+	       <input type="checkbox" id="boldCheckbox" hidden={ this.state.hidden }/>
+	       <button id="decreaseButton" hidden={ this.state.hidden }>-</button>
+	       <span id="fontSizeSpan" hidden={ this.state.hidden }>{this.props.size}</span>
+	       <button id="increaseButton" hidden={ this.state.hidden }>+</button>
+	       <span id="textSpan" onClick={this.toggle.bind(this)}>{this.props.text}</span>
 	       </div>
 	);
     }
 }
-

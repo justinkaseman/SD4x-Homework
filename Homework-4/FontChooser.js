af166fdb1c1f9ed1755dc9137c9e2c6c9d2cbe8c
Justin Kaseman <justinkaseman@live.com>
2017-11-13 19:10:45 -0800

    Finished

--- Homework-4/FontChooser.js
+++ Homework-4/FontChooser.js
@@ -2,33 +2,49 @@ class FontChooser extends React.Component {
 
     constructor(props) {
 	super(props);
-  this.state = {hidden : true, bold: this.props.bold, fontSize: this.props.size};
+  this.state = {hidden : true, bold: this.props.bold, fontSize: this.props.size, minMax: false};
     }
 
     toggleShow() {
       this.setState( { hidden: !this.state.hidden } );
     }
 
-    checkBox() {
+    checkBold() {
       this.setState( { bold: !this.state.bold } );
     }
 
     sizePlus() {
-      this.setState ( { fontSize: this.state.fontSize + 1; } )
+      if (this.state.fontSize < this.props.max) {
+      this.setState ( { fontSize: this.state.fontSize + 1 } )
+      this.setState ( { minMax: false } )
+      }
+      else if (this.state.fontSize = this.props.max) {
+      this.setState ( { minMax: true } )
+      };
     }
 
     sizeMinus() {
-      this.setState ( { fontSize: this.state.fontSize - 1; } )
+      if (this.state.fontSize > this.props.min) {
+      this.setState ( { fontSize: this.state.fontSize - 1 } )
+      this.setState ( { minMax: false } )
+      }
+      else if (this.state.fontSize = this.props.max) {
+      this.setState ( { minMax: true } )
+      };
+    }
+
+    defaultValue() {
+      this.setState( { fontSize: this.props.size });
     }
 
     render() {
       var weight = this.state.bold ? 'bold' : 'normal' ;
-      var checked = this.state.bold ? checked : '' ;
+      var minMaxColor = this.state.minMax ? 'red' : 'black' ;
 	return(
 	       <div>
-	       <input type="checkbox" id="boldCheckbox" hidden={ this.state.hidden } onClick={this.checkBox.bind(this)} {checked}/>
+	       <input type="checkbox" id="boldCheckbox" hidden={ this.state.hidden } onClick={this.checkBold.bind(this)} checked={this.state.bold} />
 	       <button id="decreaseButton" onClick={this.sizeMinus.bind(this)} hidden={ this.state.hidden }>-</button>
-	       <span id="fontSizeSpan" hidden={ this.state.hidden }>{this.state.fontSize}</span>
+	       <span id="fontSizeSpan" style=( { color: minMaxColor } ) onDoubleClick={this.defaultValue.bind(this)} hidden={ this.state.hidden }>{this.state.fontSize}</span>
 	       <button id="increaseButton" onClick={this.sizePlus.bind(this)} hidden={ this.state.hidden }>+</button>
 	       <span id="textSpan" onClick={this.toggleShow.bind(this)} style=( { fontWeight: weight, fontSize: this.state.fontSize } )>{this.props.text}</span>
 	       </div>
